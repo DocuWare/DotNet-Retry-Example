e92dd8ab49111dac1cdb2e97148b0cabbb7ce678
Florian Mittermeier <florian.mittermeier@docuware.com>
2025-02-05 18:05:26 +0100

    Add retry policy example projects and unit tests
    Introduced a new Visual Studio solution with projects targeting .NET 8, .NET Framework 4.7.2, and .NET Standard 2.0. Implemented retry policy for handling HTTP 502 and 504 errors in both .NET 8 and .NET Framework 4.7.2 projects, using a shared .NET Standard 2.0 library. Added configuration files (`App.config` and `packages.config`) for the .NET Framework 4.7.2 project. Created unit tests for the retry policy logic in a .NET 8 test project, using `Moq` and `Xunit`.

---
+++ src/retry-example-error-502-and-504/retry-example-error-502-and-504-retry-policy-dot-net-standard-2/RetryPolicy.cs
@@ -0,0 +1,44 @@
+﻿using retry_example_error_502_and_504_retry_policy_dot_net_standard_2.Interfaces;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace retry_example_error_502_and_504_retry_policy_dot_net_standard_2
+{
+    public class RetryPolicy : IRetryPolicy
+    {
+        public HttpMessageHandler CreateHttpMessageRetryHandler(int maxRetries, TimeSpan delay)
+        {
+            return new RetryHandler(maxRetries, delay);
+        }
+
+        public class RetryHandler : DelegatingHandler
+        {
+            private readonly int _maxRetries;
+            private readonly TimeSpan _delay;
+
+            public RetryHandler(int maxRetries, TimeSpan delay)
+            {
+                _maxRetries = maxRetries;
+                _delay = delay;
+            }
+
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                HttpResponseMessage response = null;
+                for (int i = 0; i < _maxRetries; i++)
+                {
+                    response = await base.SendAsync(request, cancellationToken);
+                    if (response.StatusCode != HttpStatusCode.BadGateway && response.StatusCode != HttpStatusCode.GatewayTimeout)
+                    {
+                        return response;
+                    }
+                    await Task.Delay(_delay, cancellationToken);
+                }
+                return response;
+            }
+        }
+    }
+}
